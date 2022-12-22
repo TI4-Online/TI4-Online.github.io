806c99110556a1679ae48c60d60f309bf2193d16
Darrell <darrell.anderson@gmail.com>
2022-12-22 10:17:38 -0800

    draw inside a try/finally block to make sure context gets restored

--- overlay/scripts/refresh-heartbeat.js
+++ overlay/scripts/refresh-heartbeat.js
@@ -119,103 +119,105 @@ class GameDataHeartbeat {
     }
     const ctx = this._canvas.getContext("2d");
     ctx.save();
-    ctx.scale(this._scale, this._scale);
+    try {
+      ctx.scale(this._scale, this._scale);
 
-    const w = this._w;
-    const h = this._h;
-    const now = Date.now() / 1000;
+      const w = this._w;
+      const h = this._h;
+      const now = Date.now() / 1000;
 
-    const labelWidth = 77;
-    const labelX = w - labelWidth;
-    const dataX = 4;
-    const dataWidth = labelX - dataX * 2;
+      const labelWidth = 77;
+      const labelX = w - labelWidth;
+      const dataX = 4;
+      const dataWidth = labelX - dataX * 2;
 
-    const statusToY = {
-      header: Math.floor(h / 6),
-      1: Math.floor((h * 2) / 6),
-      0: Math.floor((h * 3) / 6),
-      "-1": Math.floor((h * 4) / 6),
-      footer: Math.floor((h * 5) / 6),
-    };
-    const statusToColor = {
-      "-1": "red",
-      0: "yellow",
-      1: "green",
-    };
+      const statusToY = {
+        header: Math.floor(h / 6),
+        1: Math.floor((h * 2) / 6),
+        0: Math.floor((h * 3) / 6),
+        "-1": Math.floor((h * 4) / 6),
+        footer: Math.floor((h * 5) / 6),
+      };
+      const statusToColor = {
+        "-1": "red",
+        0: "yellow",
+        1: "green",
+      };
 
-    // Clear.
-    ctx.clearRect(0, 0, w, h);
+      // Clear.
+      ctx.clearRect(0, 0, w, h);
 
-    ctx.font = "12px Open Sans, sans-serif";
+      ctx.font = "12px Open Sans, sans-serif";
 
-    // Title.
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = this._statusColor;
-    ctx.fillText(`Gamedata: ${this._status}`, w / 2, statusToY.header);
+      // Title.
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillStyle = this._statusColor;
+      ctx.fillText(`Gamedata: ${this._status}`, w / 2, statusToY.header);
 
-    // Y axis labels.
-    ctx.textAlign = "left";
-    ctx.textBaseline = "middle";
-    ctx.fillStyle = statusToColor[-1];
-    ctx.fillText("error", labelX, statusToY[-1]);
-    ctx.fillStyle = statusToColor[0];
-    ctx.fillText("not-modified", labelX, statusToY[0]);
-    ctx.fillStyle = statusToColor[1];
-    ctx.fillText("update", labelX, statusToY[1]);
+      // Y axis labels.
+      ctx.textAlign = "left";
+      ctx.textBaseline = "middle";
+      ctx.fillStyle = statusToColor[-1];
+      ctx.fillText("error", labelX, statusToY[-1]);
+      ctx.fillStyle = statusToColor[0];
+      ctx.fillText("not-modified", labelX, statusToY[0]);
+      ctx.fillStyle = statusToColor[1];
+      ctx.fillText("update", labelX, statusToY[1]);
 
-    // X axis labels (track minutes).
-    ctx.fillStyle = "white";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    const dSeconds = new Date(now * 1000).getSeconds();
-    for (let i = -1; i <= this._historyWindowSeconds / 60; i++) {
-      const timestamp = now - dSeconds - i * 60;
-      const u = 1 - (now - timestamp) / this._historyWindowSeconds;
-      if (u > 1.03) {
-        continue; // draw text a little off to the right, but not TOO much
+      // X axis labels (track minutes).
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      const dSeconds = new Date(now * 1000).getSeconds();
+      for (let i = -1; i <= this._historyWindowSeconds / 60; i++) {
+        const timestamp = now - dSeconds - i * 60;
+        const u = 1 - (now - timestamp) / this._historyWindowSeconds;
+        if (u > 1.03) {
+          continue; // draw text a little off to the right, but not TOO much
+        }
+        const date = new Date(Math.floor(timestamp * 1000));
+        const str = date.toTimeString().substring(0, 5);
+        const x = dataX + u * dataWidth;
+        const y = statusToY.footer;
+        ctx.fillText(str, x, y);
       }
-      const date = new Date(Math.floor(timestamp * 1000));
-      const str = date.toTimeString().substring(0, 5);
-      const x = dataX + u * dataWidth;
-      const y = statusToY.footer;
-      ctx.fillText(str, x, y);
-    }
 
-    // Draw a background line tracking points.
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "gray";
-    ctx.beginPath();
-    let lastY = undefined;
-    for (const entry of this._history) {
-      const u = 1 - (now - entry.timestamp) / this._historyWindowSeconds;
-      const x = dataX + u * dataWidth;
-      const y = statusToY[entry.status];
-      if (lastY === undefined) {
-        ctx.moveTo(dataX, y);
-      }
-      ctx.lineTo(x, y);
-      lastY = y;
-    }
-    if (lastY !== undefined) {
-      ctx.lineTo(dataX + dataWidth, lastY);
-    }
-    ctx.stroke();
-
-    // Draw status colors.
-    const r = 2;
-    for (const entry of this._history) {
-      const u = 1 - (now - entry.timestamp) / this._historyWindowSeconds;
-      const x = dataX + u * dataWidth;
-      const y = statusToY[entry.status];
-
-      ctx.fillStyle = statusToColor[entry.status];
+      // Draw a background line tracking points.
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "gray";
       ctx.beginPath();
-      ctx.arc(x, y, r, 0, Math.PI * 2, true);
-      ctx.fill();
-    }
+      let lastY = undefined;
+      for (const entry of this._history) {
+        const u = 1 - (now - entry.timestamp) / this._historyWindowSeconds;
+        const x = dataX + u * dataWidth;
+        const y = statusToY[entry.status];
+        if (lastY === undefined) {
+          ctx.moveTo(dataX, y);
+        }
+        ctx.lineTo(x, y);
+        lastY = y;
+      }
+      if (lastY !== undefined) {
+        ctx.lineTo(dataX + dataWidth, lastY);
+      }
+      ctx.stroke();
 
-    ctx.restore();
+      // Draw status colors.
+      const r = 2;
+      for (const entry of this._history) {
+        const u = 1 - (now - entry.timestamp) / this._historyWindowSeconds;
+        const x = dataX + u * dataWidth;
+        const y = statusToY[entry.status];
+
+        ctx.fillStyle = statusToColor[entry.status];
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, Math.PI * 2, true);
+        ctx.fill();
+      }
+    } finally {
+      ctx.restore();
+    }
   }
 }
 
