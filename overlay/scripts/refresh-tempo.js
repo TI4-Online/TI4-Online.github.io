994400b0cab8fac728eddd9c245f31b7d1790985
Darrell <darrell.anderson@gmail.com>
2022-12-29 10:16:41 -0800

    draw points over lines, spread out point collisions

--- overlay/scripts/refresh-tempo.js
+++ overlay/scripts/refresh-tempo.js
@@ -15,8 +15,6 @@ class Tempo {
       throw new Error(`Missing element id "${elementId}"`);
     }
 
-    this._bezier = true;
-
     if (this._canvas.width === 0 && this._canvas.height === 0) {
       // Size slightly smaller to prevent growing table size, then pad
       const w = this._canvas.parentNode.offsetWidth - 4;
@@ -77,10 +75,13 @@ class Tempo {
       width: this._canvas.width - margin * 2,
       height: this._canvas.height - margin * 2,
     };
+    const maxRound = Math.max(6, currentRound);
+    const maxScore = Math.max(10, scoreboard);
 
     const ctx = this._canvas.getContext("2d");
     ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
 
+    // Axis.
     ctx.strokeStyle = "#aaa";
     ctx.lineWidth = 2;
     ctx.beginPath();
@@ -89,12 +90,22 @@ class Tempo {
     ctx.lineTo(bb.left + bb.width, bb.top + bb.height);
     ctx.stroke();
 
+    // Dotted horizontal lines.
+    ctx.setLineDash([3, 10]);
+    for (let value = 2; value <= maxScore; value += 2) {
+      const y = bb.top + ((maxScore - value) / maxScore) * bb.height;
+      ctx.beginPath();
+      ctx.moveTo(bb.left, y);
+      ctx.lineTo(bb.left + bb.width, y);
+      ctx.stroke();
+    }
+    ctx.setLineDash([]);
+
     ctx.fillStyle = "#aaa";
     ctx.font = "600 32px Open Sans, sans-serif";
 
     ctx.textAlign = "right";
     ctx.textBaseline = "middle";
-    const maxScore = Math.max(10, scoreboard);
     for (let value = 0; value <= maxScore; value += 2) {
       const y = bb.top + ((maxScore - value) / maxScore) * bb.height;
       ctx.fillText(value, bb.left - 10, y);
@@ -110,7 +121,6 @@ class Tempo {
 
     ctx.textAlign = "center";
     ctx.textBaseline = "top";
-    const maxRound = Math.max(6, currentRound);
     for (let value = 0; value <= maxRound; value += 1) {
       const x = bb.left + (value / maxRound) * bb.width;
       ctx.fillText(value, x, bb.top + bb.height + 10);
@@ -123,11 +133,17 @@ class Tempo {
     ctx.fillText("ROUND", 0, 0);
     ctx.restore();
 
+    // Compute lines for each player. Do this once, then draw lines and
+    // finally draw points OVER lines.
     const pointRadius = Math.ceil(bb.width * 0.01);
+    const pointOffset = Math.ceil(pointRadius * 2);
+    const playerColorNameToXYs = {};
     for (const colorName of colorNames) {
-      ctx.fillStyle = GameDataUtil.colorNameToHex(colorName);
-      ctx.strokeStyle = GameDataUtil.colorNameToHex(colorName);
-      const points = [[bb.left, bb.top + bb.height]];
+      playerColorNameToXYs[colorName] = [];
+    }
+    for (const colorName of colorNames) {
+      const points = playerColorNameToXYs[colorName];
+      points.push([bb.left, bb.top + bb.height]);
       for (let round = 1; round < maxRound; round++) {
         const colorNameToScore = roundToPlayerColorNameToScore[round];
         if (!colorNameToScore) {
@@ -141,28 +157,47 @@ class Tempo {
         let y = Math.floor(
           bb.top + (bb.height * (maxScore - score)) / maxScore
         );
-        points.push([x, y]);
 
-        // Draw point.
+        // How many points at this spot, and this player's index therein.
+        let hereCount = 0;
+        let hereMyIndex = 0;
+        Object.entries(colorNameToScore).forEach(
+          ([peerColorName, peerScore]) => {
+            if (peerColorName === colorName) {
+              hereMyIndex = hereCount;
+            }
+            if (peerScore === score) {
+              hereCount += 1;
+            }
+          }
+        );
+
+        // Tweak points slightly to avoid overlap.
+        const left = -((hereCount - 1) * pointOffset) / 2;
+        const offset = hereMyIndex * pointOffset;
+        x += Math.floor(left + offset);
+
+        points.push([x, y]);
+      }
+    }
+
+    // Draw lines.
+    for (const colorName of colorNames) {
+      ctx.fillStyle = GameDataUtil.colorNameToHex(colorName);
+      ctx.strokeStyle = GameDataUtil.colorNameToHex(colorName);
+      const points = playerColorNameToXYs[colorName];
+      ImageUtil.bezierCurveThrough(ctx, points);
+    }
+
+    // Draw points.
+    for (const colorName of colorNames) {
+      ctx.fillStyle = GameDataUtil.colorNameToHex(colorName);
+      ctx.strokeStyle = GameDataUtil.colorNameToHex(colorName);
+      const points = playerColorNameToXYs[colorName];
+      for (const [x, y] of points) {
         ctx.beginPath();
         ctx.arc(x, y, pointRadius, 0, Math.PI * 2, true);
         ctx.fill();
-      }
-
-      if (this._bezier) {
-        ImageUtil.bezierCurveThrough(ctx, points);
-      } else {
-        ctx.beginPath();
-        let first = true;
-        for (const point of points) {
-          if (first) {
-            first = false;
-            ctx.moveTo(point[0], point[1]);
-          } else {
-            ctx.lineTo(point[0], point[1]);
-          }
-        }
-        ctx.stroke();
       }
     }
   }
