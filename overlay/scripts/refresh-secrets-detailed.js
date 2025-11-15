d6dbde2d17d5a97b1a6bd75070541aaee9f792b4
Darrell <darrell.anderson@gmail.com>
2025-11-15 12:41:21 -0800

    comments

---
+++ overlay/scripts/refresh-secrets-detailed.js
@@ -0,0 +1,7 @@
+/**
+ * Hunter:
+ * "I'm thinking like each player has a box and the box has a row for each
+ * secret score or unscored and at the top of each box it say how many they
+ * can hold total similar to objective progress but the boxes are each player"
+ * (display max secrets per player, normally 3)
+ */
